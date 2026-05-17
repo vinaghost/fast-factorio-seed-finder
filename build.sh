09fe156b987cfcb78324c23fb1b7f0a4495357b6
VINAGHOST <nguyentrungnhan87@gmail.com>
2026-05-17 13:23:16 +0000

    build script

--- build.sh
+++ build.sh
@@ -14,10 +14,15 @@ if ! command -v cmake >/dev/null 2>&1; then
   exit 1
 fi
 
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 100
+sudo update-alternatives --auto g++
+
 echo "Configuring project..."
-cmake -S "$SOURCE_DIR" -B "$BUILD_DIR" -G "$GENERATOR" -DCMAKE_CXX_COMPILER=g++-14
+cmake -S "$SOURCE_DIR" -B "$BUILD_DIR" -G "$GENERATOR"
 
 echo "Building project..."
 cmake --build "$BUILD_DIR" --config "$CONFIG" -j
 
 echo "Build succeeded."
+
+#./build/seed_finders/solo_any/solo_any_seed_finder -o output.csv --threads 2 --last-seed 1000000
